cab75d00b3351ac46580296d9fa68a84594e8010
Victor Mayowa ADELEYE <93038708+victormayowa@users.noreply.github.com>
2023-10-24 00:56:30 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,36 +1,27 @@
 #!/usr/bin/python3
-'''Pythonic API'''
+"""Pthonic API"""
 import requests
 import sys
 
 
+def todos(id):
+    """exports"""
+    user_link = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    user = requests.get(user_link).json()
+    todo_link = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+    todo_list = requests.get(todo_link).json()
+    user = user["username"]
+    file = "{}.csv".format(id)
+    with open(file, "w") as f:
+        for todo in todo_list:
+            line = '"{}","{}","{}","{}"\n'.format(
+                id,
+                user,
+                str(todo["completed"]),
+                todo["title"]
+            )
+            f.write(line)
+
+
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = "{}/users/{}".format(base_url, employee_id)
-    tasks_url = "{}/todos?userId={}".format(base_url, employee_id)
-
-    try:
-        user_response = requests.get(user_url)
-        tasks_response = requests.get(tasks_url)
-        user_data = user_response.json()
-        tasks_data = tasks_response.json()
-        
-        if not user_data:
-            print("Employee not found")
-        else:
-            employee_name = user_data.get("name")
-            completed_tasks = [task for task in tasks_data if task.get("completed")]
-            total_tasks = len(tasks_data)
-            num_completed = len(completed_tasks)
-
-            print("Employee {} is done with tasks({}/{}):".format(employee_name, num_completed, total_tasks))
-            for task in completed_tasks:
-                print("\t {}".format(task.get("title")))
-
-    except Exception as e:
-        print("An error occurred: {}".format(e))
+    todos(sys.argv[1])
