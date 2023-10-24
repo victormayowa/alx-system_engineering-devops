d6e70f70956f15ac07f752d6f229670f9d5d1b6a
Victor Mayowa ADELEYE <93038708+victormayowa@users.noreply.github.com>
2023-10-24 01:28:36 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+"""#!/usr/bin/python3
 '''Pythonic API'''
 import requests
 import sys
@@ -34,4 +34,32 @@ if __name__ == "__main__":
         sys.exit(1)
 
     employee_id = sys.argv[1]
-    get_todo_progress(employee_id)
+    get_todo_progress(employee_id)"""
+
+#!/usr/bin/python3
+"""gets todo info and export it to a csv file"""
+import requests
+import sys
+
+
+def export_todo(id):
+    """exports todo to a csv"""
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    user = requests.get(user_url).json()
+    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+    todos = requests.get(todo_url).json()
+    user_name = user["username"]
+    file_name = "{}.csv".format(id)
+    with open(file_name, "w") as f:
+        for todo in todos:
+            row = '"{}","{}","{}","{}"\n'.format(
+                id,
+                user_name,
+                str(todo["completed"]),
+                todo["title"]
+            )
+            f.write(row)
+
+
+if __name__ == "__main__":
+    export_todo(sys.argv[1])
