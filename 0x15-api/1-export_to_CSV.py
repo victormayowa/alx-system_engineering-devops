03e715e32f9b4298b1c290b306e0a29a3a6b0a16
Victor Mayowa ADELEYE <93038708+victormayowa@users.noreply.github.com>
2023-10-24 01:16:41 +0100

    Update 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''export to csv'''
+import requests
+import sys
+import csv
+
+
+def export_to_csv(employee_id):
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+
+    user_response = requests.get(user_url)
+    todo_response = requests.get(todo_url)
+
+    if user_response.status_code != 200 or todo_response.status_code != 200:
+        print("Error: Unable to fetch data from the API.")
+        sys.exit(1)
+
+    user_data = user_response.json()
+    todo_data = todo_response.json()
+
+    employee_id = user_data["id"]
+    employee_name = user_data["username"]
+
+    file_name = f"{employee_id}.csv"
+
+    with open(file_name, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
+
+        for task in todo_data:
+            task_id = task["id"]
+            completed = task["completed"]
+            title = task["title"]
+
+            writer.writerow([employee_id, employee_name, completed, title])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    export_to_csv(employee_id)
